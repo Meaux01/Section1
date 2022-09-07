17bc9338b663625e9b2e8beee301df06cf965b7f
meaux21 <maurice.grimes21@gmail.com>
2022-09-07 16:44:54 -0400

    Fizz Buzz Answered2

--- answers.js
+++ answers.js
@@ -27,11 +27,11 @@ for(i=1; i<=100 ;i++)
     }
     else if(i%3==0)
     {
-        console.log(Fizz)
+        console.log("Fizz")
     }
     else if(i%5==0)
     {
-        console.log(Buzz)
+        console.log("Buzz")
     }
     else
     {
