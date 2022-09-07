e07415245c05aa722b161b30fda9d6eb1572ba2c
meaux21 <maurice.grimes21@gmail.com>
2022-09-07 16:49:56 -0400

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -42,6 +42,20 @@ for(i=1; i<=100 ;i++)
 
 ////////////////////////////////
 // Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+plantee.splice(2,1, 5001)
+console.log(plantee)
+wolfy.splice(3,1, "Gotham City")
+console.log(wolfy)
+dart.push("Hawkins")
+console.log(dart)
+wolfy.shift()
+wolfy.unshift("Gameboy")
+console.log(wolfy)
 ////////////////////////////////
 
 ////////////////////////////////
